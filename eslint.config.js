24b5d1f88d2bca9ee408a84bdbf55c4d4e0c68d2
혜미 <tomboy.kr@gmail.com>
2025-03-03 22:06:41 +0900

    🎉 init: 초기 환경 구성 추가
    - eslint 규칙 추가
    - tsconfig 별칭 추가
    - husky 패키지 설치
    - clsx 패키지 설치
    - vite plugin tailwindcss 추가
    - vscode setting.json 추가
    
    - normalize 외 common css 추가
    - 웹폰트 추가
    - ts-merge utils 추가
    - index.html 수정
    
    #1

--- eslint.config.js
+++ eslint.config.js
@@ -32,11 +32,7 @@ export default tseslint.config(
     },
   },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked],
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -48,9 +44,42 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
-      'react-refresh/only-export-components': [
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'jsx-a11y/alt-text': [
         'warn',
-        { allowConstantExport: true },
+        {
+          elements: ['img'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn', // 유효한 aria-* 속성만 사용
+      'jsx-a11y/aria-proptypes': 'warn', // 유효한 aria-* 상태/값만 사용
+      'jsx-a11y/aria-unsupported-elements': 'warn', // DOM에서 지원되는 role, ARIA만 사용
+      'jsx-a11y/role-has-required-aria-props': 'warn', // 필수 ARIA 속성이 빠져있는지 체크
+      'jsx-a11y/role-supports-aria-props': 'warn', // ARIA 속성은 지원되는 role에서만 사용
+      'react/no-unknown-property': 'off', // DOM에 정의되지 않은 속성을 사용했는지 체크 (emotion css 속성 등 예외 케이스가 있으므로 기본은 off)
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        },
+        {
+          selector: 'function',
+          format: ['camelCase', 'PascalCase'],
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+      ],
+      'no-relative-import-paths/no-relative-import-paths': [
+        // 같은 폴더인 경우를 제외하고 import 경로는 항상 절대 경로를 사용
+        'warn',
+        { allowSameFolder: true, rootDir: 'src', prefix: '@' },
       ],
     },
   }
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,29 +1,16 @@
 module.exports = {
-  // 화살표 함수 식 매개변수 () 생략 여부 (ex: (a) => a)
-  arrowParens: "always",
-  // 닫는 괄호(>) 위치 설정
-  // ex: <div
-  //       id="unique-id"
-  //       class="contaienr"
-  //     >
-  htmlWhitespaceSensitivity: "css",
-  bracketSameLine: false,
-  // 객체 표기 괄호 사이 공백 추가 여부 (ex: { foo: bar })
-  bracketSpacing: true,
-  // 행폭 설정 (줄 길이가 설정 값보다 길어지면 자동 개행)
-  printWidth: 80,
-  // 산문 래핑 설정
-  proseWrap: "preserve",
-  // 객체 속성 key 값에 인용 부호 사용 여부 (ex: { 'key': 'xkieo-xxxx' })
-  quoteProps: "as-needed",
-  // 세미콜론(;) 사용 여부
-  semi: true,
-  // 싱글 인용 부호(') 사용 여부
-  singleQuote: true,
-  // 탭 너비 설정
-  tabWidth: 2,
-  // 객체 마지막 속성 선언 뒷 부분에 콤마 추가 여부
-  trailingComma: "es5",
-  // 탭 사용 여부
-  useTabs: false,
+  arrowParens: 'always', // 화살표 함수 식 매개변수 () 생략 여부 (ex: (a) => a)
+  htmlWhitespaceSensitivity: 'css',
+  bracketSameLine: false, // 닫는 괄호(>) 위치 설정
+  bracketSpacing: true, // 객체 표기 괄호 사이 공백 추가 여부 (ex: { foo: bar })
+  printWidth: 200, // 행폭 설정 (줄 길이가 설정 값보다 길어지면 자동 개행)
+  proseWrap: 'preserve', // 산문 래핑 설정
+  quoteProps: 'as-needed', // 객체 속성 key 값에 인용 부호 사용 여부 (ex: { 'key': 'xkieo-xxxx' })
+  semi: true, // 세미콜론(;) 사용 여부
+  singleQuote: true, // 싱글 인용 부호(') 사용 여부
+  jsxSingleQuote: false,
+  tabWidth: 2, // 탭 너비 설정
+  trailingComma: 'es5', // 객체 마지막 속성 선언 뒷 부분에 콤마 추가 여부
+  useTabs: false, // 탭 사용 여부
+  plugins: ['prettier-plugin-tailwindcss'],
 };
