9ab1701bf3fc98ddcafd965b86724590b3ec63fc
혜미 <tomboy.kr@gmail.com>
2025-03-04 16:52:53 +0900

    🎨 feat: input field 컴포넌트 추가
    - normalize.css 삭제(tailwind built in)
    - primary color token 추가
    
    #2

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,32 @@
-function Input() {
-  return <div>Input</div>;
+import { tm } from "@/utils/ts-merge";
+import { ComponentProps, useId } from "react";
+
+type InputTextProps = ComponentProps<'input'> & {
+  type?: string;
+  labelText: string;
+  labelHidden?: boolean;
+  defaultValue?: string;
+  placeholder?: string;
 }
-export default Input;
+
+function InputText({ type = 'text', labelText, labelHidden = false, ...restProps }:InputTextProps) {
+  const inputId = useId();
+
+  return (
+    <div className="flex flex-col gap-y-[6px] text-primary">
+      <label htmlFor={inputId} className={
+        tm(
+          {"sr-only" : labelHidden},
+          "text-xs leading-[16px]"
+        )
+      }>{labelText}</label>
+      <input type={type} id={inputId} {...restProps} className={
+        tm(
+        "w-full py-[10px] px-2",
+        "border border-primary",
+        "text-xs leading-none rounded-[10px]"
+      )} />
+    </div>
+  );
+}
+export default InputText;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import InputText from '@/components/Input';
 
 const root = document.getElementById('root');
 
@@ -8,7 +9,13 @@ if (root) {
 
   reactDOMRoot.render(
     <StrictMode>
-      <div className="flex h-[100vh] items-center justify-center bg-[#3E3232] p-5 text-6xl text-white">말랑콩떡</div>
+      <section className="p-4">
+        <h1 className="pb-4 text-2xl">공통 컴포넌트</h1>
+        <article className="flex flex-col gap-y-2">
+          <InputText labelText="아이디" defaultValue="기본 값" placeholder="placeholder" />
+          <InputText labelText="비밀번호" type="password" labelHidden={true} />
+        </article>
+      </section>
     </StrictMode>
   );
 }
