ee3427e3f5846f165b5e6bf9c76ed1bfa1ba5760
혜미 <tomboy.kr@gmail.com>
2025-03-06 17:40:36 +0900

    🐛 fix: Switch label 타입 추가
    #27

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -3,29 +3,29 @@ import { ComponentProps, useId } from 'react';
 
 type SwitchProps = ComponentProps<'input'> & {
   label: string;
+  labelHidden?: boolean | undefined;
 };
 
-function Switch({ label, ...restProps }: SwitchProps) {
+function Switch({ label, labelHidden, ...restProps }: SwitchProps) {
   const id = useId();
+  console.log(label, labelHidden);
 
   return (
-    <div className="relative inline-flex h-[15px] items-center justify-center">
+    <label htmlFor={id} className="relative inline-flex h-[15px] items-center justify-center gap-x-2">
+      <span className={tm('text-primary text-xs leading-4', { 'sr-only': labelHidden })}>{label}</span>
       <input type="checkbox" id={id} className="peer absolute inset-0 cursor-pointer appearance-none" {...restProps} />
-      <label
-        htmlFor={id}
+      <span
         className={tm(
           'bg-brown-100 h-[7px] w-7 rounded-sm transition-[background]',
-          'before:absolute before:top-[50%] before:left-0 before:inline-block',
+          'before:absolute before:top-[50%] before:inline-block',
           'before:h-[15px] before:w-[15px] before:-translate-y-[50%]',
           'before:bg-brown-200 pointer-events-none before:rounded-lg',
-          'before:transition-[left]',
+          'before:transition-[translate]',
           'peer-checked:bg-beige-400',
           'peer-checked:before:bg-peach-800',
-          'peer-checked:before:left-[.8125rem]'
-        )}>
-        <span className="sr-only">{label}</span>
-      </label>
-    </div>
+          'peer-checked:before:translate-x-[.8125rem]'
+        )}></span>
+    </label>
   );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -45,7 +45,10 @@ function Components() {
 
       <article className="mt-15">
         <h2>Switch</h2>
-        <Switch label="" />
+        <div className="flex flex-col items-start gap-y-3">
+          <Switch label="다크모드" defaultChecked labelHidden />
+          <Switch label="혼자볼래" />
+        </div>
       </article>
 
       <article className="mt-12 flex flex-col items-start gap-y-2">
