92b3264feccff704323213a4cab40bca2da89d44
혜미 <tomboy.kr@gmail.com>
2025-03-09 16:42:33 +0900

    🎨 feat: 일기 쓰기 페이지 추가
    - DB 연동 X
    
    #38

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
-import SignInPage from '@/pages/SignIn/Index';
 import { Route, Routes } from 'react-router';
-import Components from './components';
 import ProfilePage from '@/pages/Profile/Index';
+import SignInPage from '@/pages/SignIn/Index';
+import DiaryWrite from '@/pages/DiaryWrite/';
+import Components from './components';
 
 export const navList = [
   { path: '/signin', text: '로그인', element: <SignInPage /> },
+  { path: '/diaryWrite', text: '일기 쓰기', element: <DiaryWrite /> },
   { path: '/components', text: '컴포넌트 모음', element: <Components /> },
   { path: '/profile', text: '프로필', element: <ProfilePage /> },
 ];
