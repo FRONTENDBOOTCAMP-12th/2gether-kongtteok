c2e9b45bf2a414e106d43d5ea2f718e1a0d5c254
혜미 <tomboy.kr@gmail.com>
2025-03-10 14:14:15 +0900

    🔥 remove: EmotionButton 컴포넌트 파일명 변경

--- src/components/EmotionButton.tsx
+++ src/components/EmotionButton.tsx
@@ -1,13 +1,14 @@
 import { ComponentProps } from 'react';
 import EmotionImage, { type EmotionProps } from './EmotionImage';
+import { tm } from '@/utils/ts-merge';
 
 type EmotionButtonProps = ComponentProps<'button'> & Pick<EmotionProps, 'emotion'>;
 
-function EmotionButton({ emotion = 'exciting', ...restProps }: EmotionButtonProps) {
+function EmotionButton({ emotion = 'exciting', className, ...restProps }: EmotionButtonProps) {
   return (
-    <button type="button" className="cursor-pointer" {...restProps}>
+    <button type="button" className={tm('cursor-pointer', className)} {...restProps}>
       <span className="sr-only">감정 선택</span>
-      <EmotionImage emotion={emotion} />
+      <EmotionImage emotion={emotion} title={emotion} />
     </button>
   );
 }
