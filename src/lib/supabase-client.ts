967a57291b332c3dd04afe87bf86c13fa412fbd8
peachily <peachily1108@gmail.com>
2025-03-12 17:05:06 +0900

    💄 style: switch 컴포넌트 수정 #67

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -3,11 +3,9 @@ import { Database, Tables, TablesInsert } from './database.types';
 
 const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } = import.meta.env;
 
-const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
-export default supabase;
+export const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
 
 export const DATABASE_NAME = 'diary';
 
-// 테이블에 접근하기 위한 단축키 제공
 export type DiaryItem = Tables<'diary'>;
 export type DiaryItemInsert = TablesInsert<'diary'>;
