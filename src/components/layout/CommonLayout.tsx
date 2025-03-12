0c564bc69ec0ecabdd5710990abe289a54659c13
wnsrl7250 <wnsrl7250@naver.com>
2025-03-12 22:53:41 +0900

    🎨 feat: 공통 컴포넌트 개발

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,34 @@
+import Footer from './Footer';
+import Header from './Header';
+
+type CommonLayoutProps = {
+  children: React.ReactNode;
+  headerProps?: {
+    title: string;
+    isLeftIcon?: boolean;
+    isRightIcon?: boolean;
+  };
+  showFooter?: boolean;
+};
+
+function CommonLayout({ children, headerProps, showFooter = true }: CommonLayoutProps) {
+  let paddingClasses = '';
+
+  if (headerProps && showFooter) {
+    paddingClasses = 'px-4 pb-20';
+  } else if (!showFooter) {
+    paddingClasses = 'px-4 pb-4';
+  } else {
+    paddingClasses = 'p-4';
+  }
+
+  return (
+    <div className="">
+      {headerProps && <Header {...headerProps} />}
+      <main className="p-4">{children}</main>
+      {showFooter && <Footer />}
+    </div>
+  );
+}
+
+export default CommonLayout;
