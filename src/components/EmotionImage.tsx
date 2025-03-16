fa2ce47474155bcfbc1283045cb1cbde39deb126
혜미 <tomboy.kr@gmail.com>
2025-03-16 23:48:49 +0900

    📝 docs: allowExportNames 목록 추가(EMOTION)
    - EMOTION enum 수정정

--- src/components/EmotionImage.tsx
+++ src/components/EmotionImage.tsx
@@ -1,20 +1,19 @@
 import { ComponentProps } from 'react';
 
 export type EmotionType = 'exciting' | 'happy' | 'proud' | 'fine' | 'angry' | 'tired' | 'sad' | 'depressed';
-export enum EMOTION {
-  exciting = '설렘',
-  happy = '행복',
-  proud = '뿌듯',
-  fine = '괜찮음',
-  angry = '화남',
-  tired = '피곤함',
-  sad = '슬픔',
-  depressed = '우울',
-}
+export const EMOTION = {
+  exciting: '설렘',
+  happy: '행복',
+  proud: '뿌듯',
+  fine: '괜찮음',
+  angry: '화남',
+  tired: '피곤함',
+  sad: '슬픔',
+  depressed: '우울',
+};
 
 export type EmotionProps = ComponentProps<'img'> & {
   emotion?: EmotionType;
-  emotionValue?: string;
 };
 
 function EmotionImage({ emotion = 'exciting', ...restProps }: EmotionProps) {
