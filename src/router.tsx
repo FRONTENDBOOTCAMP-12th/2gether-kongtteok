5fe2714822ba3ded9e5159704d20935413af6040
peachily <peachily1108@gmail.com>
2025-03-20 10:34:00 +0900

    🎨 feat: 프로필 편집 페이지 추가 #137

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ import DiaryView from '@/pages/DiaryView';
 import DiaryWrite from '@/pages/DiaryWrite';
 import Settings from '@/pages/Settings';
 import DiaryDetail from '@/pages/DiaryDetail';
+import ProfileEdit from '@/pages/ProfileEdit';
 import Components from '@/components';
 
 export const navList = [
@@ -18,6 +19,7 @@ export const navList = [
   { path: 'settings', text: '설정', element: <Settings /> },
   { path: 'diary/view/:diaryId', text: '내 일기 보기', element: <DiaryView /> },
   { path: 'diary/detail/:diaryId', text: '일기 상세 보기', element: <DiaryDetail /> },
+  { path: 'profile/edit', text: '프로필 편집', element: <ProfileEdit /> },
   { path: 'components', text: '컴포넌트 모음', element: <Components /> },
 ];
 
