db5ad2c69d5715ee58fadb089d5e2088a6ca50b9
혜미 <tomboy.kr@gmail.com>
2025-03-21 09:16:42 +0900

    🎨 feat: supabase 로그인 시 스토리지에 저장 안하는 옵션 추가가

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -3,9 +3,14 @@ import { Database, Tables, TablesInsert } from './database.types';
 
 const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } = import.meta.env;
 
-const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
+const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: false, //세션을 로컬 스토리지에 저장하지 않음
+  },
+});
 export default supabase;
 
+export const AUTH_STORAGE = 'kong-tteok/auth';
 export const DATABASE_NAME = 'diary';
 export const STORAGE_NAME = 'images';
 
