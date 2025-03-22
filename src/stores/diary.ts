4e5416cb9584bbb5e15db612106110f71ba8265c
혜미 <tomboy.kr@gmail.com>
2025-03-22 16:58:37 +0900

    🎨 feat: 당일 일기 작성 유무 전역 상태 추가
    - 당일 일기를 썼는데 일기 쓰기 버튼을 누르면 modal 띄움
    
    #167

---
+++ src/stores/diary.ts
@@ -0,0 +1,32 @@
+import supabase, { DATABASE_NAME } from '@/lib/supabase-client';
+import { getDate } from '@/utils/get-date';
+import { create } from 'zustand';
+import { combine } from 'zustand/middleware';
+
+interface DiaryType {
+  hasTodayPost: boolean;
+}
+
+interface DiaryActions {
+  checkTodayPost: (userId: string) => void;
+}
+
+const initialDiary: DiaryType = {
+  hasTodayPost: false,
+};
+
+export const useDiaryStore = create(
+  combine(
+    { ...initialDiary },
+    (set): DiaryActions => ({
+      checkTodayPost: async (userId) => {
+        const post = await supabase.from(DATABASE_NAME).select('*').eq('user_id', userId).eq('date', getDate());
+        const hasPost = !!post?.data?.length;
+
+        set(() => {
+          return { hasTodayPost: hasPost };
+        });
+      },
+    })
+  )
+);
