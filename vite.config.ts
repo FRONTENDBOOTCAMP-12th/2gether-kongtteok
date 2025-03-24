6a3c229b5df34bae2f17e09b7a3e354274f55ec5
혜미 <tomboy.kr@gmail.com>
2025-03-24 09:50:18 +0900

    🐳 chore: 최적화 설정
    - @prerenderer/renderer-puppeteer 설치
    - @prerenderer/rollup-plugin 설치
    
    #203

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,51 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import prerender from '@prerenderer/rollup-plugin';
 
-const viteConfig = defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic',
-    }),
-    tailwindcss(),
-  ],
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
-  preview: {
-    host: 'localhost',
-    port: 8080,
-  },
-  css: {
-    devSourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+const viteConfig = defineConfig((env) => {
+  const isDevMode = env.mode.includes('development');
+
+  return {
+    plugins: [
+      react({
+        jsxRuntime: 'automatic',
+      }),
+      tailwindcss(),
+      prerender({
+        routes: ['/'], // 정적 페이지를 생성할 루트(경로) 추가
+        renderer: '@prerenderer/renderer-puppeteer',
+        postProcess(renderedRoute) {
+          // HTTP → HTTPS로 변경 or `localhost` → 서비스 URL로 변경
+          renderedRoute.html = renderedRoute.html
+            .replace(/http:/gi, 'https:')
+            .replace(/(https:\/\/)?(localhost|127\.0\.0\.1):\d*/gi, process.env.CI_ENVIRONMENT_URL || '');
+        },
+      }),
+      ViteImageOptimizer({
+        cache: true,
+        cacheLocation: '.cache',
+      }),
+    ],
+    server: {
+      host: 'localhost',
+      port: 3000,
     },
-  },
+    preview: {
+      host: 'localhost',
+      port: 8080,
+    },
+    css: {
+      devSourcemap: true,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 });
 
 export default viteConfig;
